0221813a3ce6846f1535937c04069c8c01df4aeb
jhersbo <jhersbo@gmail.com>
2022-05-05 12:46:49 -0500

    moar

--- src/components/LoginRegScreen.js
+++ src/components/LoginRegScreen.js
@@ -4,13 +4,15 @@ import { useState } from "react"
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Avatar, Button, Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
 
 export default function LoginRegScreen( {user, setUser, userDB, setUserDB, thinScreen} ){
     
     let [failedLogin, setFailedLogin] = useState(false)
     let [username, setUsername] = useState(undefined)
     let [password, setPassword] = useState(undefined)
+    let [editing, setEditiing] = useState(false)
     
     async function handleSubmit(){
         let usernamesInDB = userDB.map(user => user.username)
@@ -32,25 +34,43 @@ export default function LoginRegScreen( {user, setUser, userDB, setUserDB, thinS
     if(thinScreen){
         return(
             <div>
-                {/* {user? */}
-                <form className='login-form' style={{'justifyContent': 'center', 'marginTop': '5%', 'display': 'flex', 'flexDirection': 'column','padding': '5%'}}>
-                    <TextField
-                    id='outlined-required'
-                    required
-                    label='Username'
-                    defaultValue={username}
-                    margin='none'
-                    onChange={(e)=>{setUsername(e.target.value)}}
-                    ></TextField>
-                    <TextField
-                    id='outlined-required'
-                    required
-                    label='Password'
-                    margin='normal'
-                    onChange={(e)=>{setPassword(e.target.value)}}
-                    ></TextField>
-                    <Button variant='contained' onClick={()=>{handleSubmit()}}>Login</Button>
-                </form>
+                {!user? 
+                    <form className='login-form' style={{'justifyContent': 'center', 'marginTop': '5%', 'display': 'flex', 'flexDirection': 'column','padding': '5%'}}>
+                        <TextField
+                        id='outlined-required'
+                        required
+                        label='Username'
+                        defaultValue={username}
+                        margin='none'
+                        onChange={(e)=>{setUsername(e.target.value)}}
+                        ></TextField>
+                        <TextField
+                        id='outlined-required'
+                        required
+                        label='Password'
+                        margin='normal'
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                        ></TextField>
+                        <Button variant='contained' onClick={()=>{handleSubmit()}}>Login</Button>
+                    </form>
+                    :
+                    <div style={{'textAlign': 'center','marginTop': '5%', 'padding': '5%'}}>
+                        <Card sx={{ maxWidth: .90 * (window.innerWidth)}}>
+                            <CardHeader 
+                            avatar={
+                                <Avatar sx={{ bgcolor: red[300] }}>{user.firstname.charAt(0)}</Avatar>
+                            }
+                            title={user.firstname + ' ' + user.lastname}
+                            subheader={user.username}>
+                            </CardHeader>
+                            <CardContent>
+                                <Typography>
+                                    
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                }
                 
             </div>
         )
